d77e3fe41bdd6ee43ee9b939a0e54ddbd86f81a1
Daniel Oladele <danieloladele7@gmail.com>
2022-06-03 04:12:35 -0700

    Algorithms in C using Variables, if, else, while and for

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,11 +16,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n < 0)
-		printf("%d is %s ", n, "negative");
+		printf("%d is %s\n", n, "negative");
 	else if (n > 0)
-		printf("%d is %s ", n, "positive");
+		printf("%d is %s\n", n, "positive");
 	else
-		printf("%d is %s ", n, "zero");
+		printf("%d is %s\n", n, "zero");
 
 	return (0);
 }
