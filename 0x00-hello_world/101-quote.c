f98cd491aa1f2515beac7b345c4fc23e1d409660
Daniel Oladele <danieloladele7@gmail.com>
2022-06-02 20:31:02 -0700

    advanced added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,8 @@
  */
 int main(void)
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
 	write(2, ch, sizeof(ch));
 	return (1);
 }
