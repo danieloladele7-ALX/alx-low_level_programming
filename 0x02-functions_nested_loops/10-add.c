28c54c071e9cff40d7babd32ae5146376dfb5b21
Daniel Oladele <danieloladele7@gmail.com>
2022-06-07 21:04:13 -0700

    c functions and while loop putchar.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * add int to int - function that adds two integers
- *
+ * add(int, int) - function that adds two integers
+ *@n: integer
  * Return: sum
  */
 int add(int a, int b)
 {
-	return a+b;
+	return (a + b);
 }
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,8 @@
 #include  <stdio.h>
 #include "main.h"
 /**
- * print_to_98 - function that prints all natural numbers from n to 98, followed
- * by a new line
+ * print_to_98 - function that prints all natural numbers from n to 98,
+ * followed by a new line
  * @n: integer
  * Return: Nothing
  */
