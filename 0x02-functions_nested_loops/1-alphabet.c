98b472dfea7775283477a07e321a1541c2f2ef04
Daniel Oladele <danieloladele7@gmail.com>
2022-06-07 20:18:23 -0700

    c functions and while loop putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the english alphabet from a-z.
+ *
+ * Return: Nothing.
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar ('\n');
+}
