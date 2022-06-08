4bd62138acaf6ada2485b255258f60229af16644
Daniel Oladele <danieloladele7@gmail.com>
2022-06-07 19:44:17 -0700

    c functions and while loop putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,15 @@
 #include "main.h"
-/**
- * main - Prints _putchar
- * 
- * Return: Always 0 (Success)
- */
+
 int main(void)
 {
 	char *ptr = "_putchar";
-	
+
 	while (*ptr)
 	{
 		_putchar(*ptr);
 		ptr++;
 	}
 	_putchar("\n");
-	
+
 	return (0);
 }
