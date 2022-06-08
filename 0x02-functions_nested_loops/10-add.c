429dccb09630099fc87c8c733b6edf500ff7a5da
Daniel Oladele <danieloladele7@gmail.com>
2022-06-07 20:57:01 -0700

    c functions and while loop putchar.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add int to int - function that adds two integers
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return a+b;
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,6 +11,7 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int a, int b);
 void print_to_98(int n);
 
 #endif /*MAIN_H*/
