227173719b6519e6a5bf77b8ced65040bd0f0f86
Daniel Oladele <danieloladele7@gmail.com>
2022-06-09 20:26:12 -0700

    More Nested Loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,42 +7,16 @@
  */
 void print_number(int n)
 {
-	int l; /* power of 10 */
-	int c; /* boolean check */
-	int num; /* convert int to long */
-	char hyphen = 45;
+	unsigned int i = n;
 
-	num = n;
-	/* negatives */
-	if (num < 0)
+	if (n < 0)
 	{
-		num *= -1;
-		_putchar(hyphen);
+		_putchar(45);
+		i = -i;
 	}
-
-	/* count up */
-	l = 1;
-	c = 1;
-	while (c)
+	if (i / 10)
 	{
-		if (num / (l * 10) > 0)
-			l *= 10;
-		else
-			c = 0;
+		print_number(i / 10);
 	}
-
-	/* count down */
-	while (num >= 0)
-	{
-		if (l == 1)
-		{
-			_putchar(num % 10 + '0');
-			num = -1;
-		}
-		else
-		{
-			_putchar((num / l % 10) + '0');
-			l /= 10;
-		}
-	}
+	_putchar(i % 10 + '0');
 }
