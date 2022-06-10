ce5da97a050fa99fd9dbe8201a2881f1f1294502
Daniel Oladele <danieloladele7@gmail.com>
2022-06-09 18:25:16 -0700

    0x03-Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function for is positive or negative
+ * to check if i=0 works correctly.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
