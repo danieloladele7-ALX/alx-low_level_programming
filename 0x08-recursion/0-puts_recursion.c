905e3fbbf0911aef7bd2d95a4d1b85abe9b8b429
Daniel Oladele <danieloladele7@gmail.com>
2022-06-22 19:50:18 -0700

    0x08-recursion mandatory section

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,7 +7,9 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
+		_putchar('\n);
+	else
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,9 +7,11 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
+		_putchar('\n');
+	else
 	{
-		_puts_recursion(s + 1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
