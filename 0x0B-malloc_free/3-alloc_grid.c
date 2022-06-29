93b04ed3a5f41ecfa9e5c0b9f7b272f99cdde8a1
Daniel Oladele <danieloladele7@gmail.com>
2022-06-28 20:53:36 -0700

    mandatory 2 to 4

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	matrix = (int *) malloc(height * sizeof(int *));
+	matrix = (int **) malloc(height * sizeof(int *));
 
 	if (matrix == NULL)
 		return (NULL);
