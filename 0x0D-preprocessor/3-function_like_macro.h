a3af404f414e8978bbb14963b8d34f0e50a157fd
Daniel Oladele <danieloladele7@gmail.com>
2022-07-04 12:39:20 -0700

    0x0D macros

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
+
+/* Using a ternary operator to define a ABS function as a macros */
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+
+#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
