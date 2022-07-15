0a8a502b2df7b671b0dd9988faffac630ae9ef82
Daniel Oladele <danieloladele7@gmail.com>
2022-07-14 17:47:34 -0700

    Task 3 correction

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -20,7 +20,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	/* allocate memory to heap */
 	new_node = malloc(sizeof(size_t));
-	if (temp == NULL)
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
