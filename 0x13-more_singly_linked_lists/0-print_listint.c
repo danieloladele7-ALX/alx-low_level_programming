071cc7655eddf2a93330b0983d3d136f5dfe4ef6
Daniel Oladele <danieloladele7@gmail.com>
2022-07-19 16:32:50 -0700

    task 0 and 1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t node;
+	size_t node = 0;
 
-	for (node = 0; h; node++)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
+		node++;
 	}
 	return (node);
 }
