1fa17c8a3df433b310927fbfde5b426d41072b46
Daniel Oladele <danieloladele7@gmail.com>
2022-07-19 18:31:19 -0700

    Advanced Tasks 100 and 101

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a linked list, safely
+ * @head: list of type listint_t to print
+ *
+ * Return: number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t node;
+	long int diff;
+
+	for (node = 0; head; node++)
+	{
+		diff = head - head->next;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+	}
+
+	return (node);
+}
