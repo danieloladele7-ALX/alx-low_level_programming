382c2909126d699c6a5548eaadda0f88347b4801
Daniel Oladele <danieloladele7@gmail.com>
2022-07-19 21:58:34 -0700

    removed white spaces lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -11,14 +11,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
-
 int _putchar(char);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
