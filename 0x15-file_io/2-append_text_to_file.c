80bccc6323384aa5f8dc0bcac312f307dfd9c2d1
Daniel Oladele <danieloladele7@gmail.com>
2022-07-25 20:48:52 -0700

    Task 2 and 3 Correction

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * create_file - creates a file and fills it with text
- * @filename: name of the file to create
- * @text_content: text to write in the file
+ * append_text_to_file - appends text at the end of a file
+ * @filename: file to append the text to
+ * @text_content: content to append into the file
  *
- * Return: 1 on success, -1 on failure
+ * Return: 1 on success and -1 on failure
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t str_len, len_write;
+	ssize_t len_write, str_len;
 
 	if (!filename)
 		return (-1);
@@ -23,11 +23,11 @@ int create_file(const char *filename, char *text_content)
 		/* get string length  */
 		for (str_len = 0 ; text_content[str_len]; str_len++)
 			;
-		/* write text to file */
 		len_write = write(fd, text_content, str_len);
 		if (len_write != str_len)
 			return (-1);
 	}
+
 	close(fd);
 
 	return (1);
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,19 @@
 #include "main.h"
 /**
+ * check_argc - checks for the correct number of arguments
+ * @argc: number of arguments
+ *
+ * Return: void
+ */
+void check_argc(int argc)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+}
+/**
  * main - copies the content of a file to another file
  * @argc: number of arguments passed to the program
  * @argv: array of arguments
@@ -10,25 +24,17 @@ int main(int argc, char *argv[])
 {
 	int file_from, file_to, read_f, close_r, close_w;
 	char buf[BUFSIZ];
-	/* Check for Correct argument */
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+
+	check_argc(argc);
 	file_from = open(argv[1], O_RDONLY);
-	/* Check that file_from exist */
 	if (file_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	/* note: 0664 (R & W usr and Grp...) represents file permission. */
-	/* Same as S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH */
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((read_f = read(file_from, buf, BUFSIZ)) > 0)
 	{
-		/* Check that file_to exist if not, close both files*/
 		if (file_to == -1 || write(file_to, buf, read_f) != read_f)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
