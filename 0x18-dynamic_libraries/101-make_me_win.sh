267748b78724d152968c6a6bc2a56de2e590d71c
Daniel <danieloladele7@gmail.com>
2022-09-06 05:28:56 +0200

    101 task correction

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,2 @@
-wget -O /tmp/win.so https://github.com/danieloladele7/alx-low_level_programming.git/raw/master/0x18-dynamic_libraries/win.so
+wget -P /tmp/win.so https://github.com/danieloladele7/alx-low_level_programming.git/raw/master/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=/tmp/win.so
